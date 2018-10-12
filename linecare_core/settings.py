8525cc6d162126a216990f6c08db321b64ecc913
Jorge Cosgayon <jorge.cosgayon@gmail.com>
2018-10-12 14:41:11 +0800

    Added testing.py settings

--- linecare_core/settings.py
+++ linecare_core/settings.py
@@ -4,7 +4,7 @@ from split_settings.tools import optional, include
 if path.isfile("/home/ubuntu/linecare/envars/local"):
     include("settings_collection/local.py")
 elif path.isfile("/home/ubuntu/linecare/envars/testing"):
-    include("settings_collection/tests.py")
+    include("settings_collection/testing.py")
 elif path.isfile("/home/ubuntu/linecare/envars/beta"):
     include("settings_collection/beta.py")
 else:
