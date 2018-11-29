a74398ea9855c4ac70be4c277546d7e4c108b0bf
Jorge Cosgayon <jorge.cosgayon@gmail.com>
2018-11-29 16:46:49 +0800

    Receptionist connect api

--- receptionist_profiles/urls.py
+++ receptionist_profiles/urls.py
@@ -12,6 +12,9 @@ urlpatterns = []
 version = 'api/v1'
 
 urlpatterns += [
+    path(f'{version}/private/connection/create',
+         receptionist_profile_api.ApiPrivateReceptionistConnectionCreate.as_view(),
+         name='api_private_receptionist_connection_create'),
     path(f'{version}/private/profile/create_by_doctor',
          receptionist_profile_api.ApiPrivateReceptionistProfileCreateByDoctor.as_view(),
          name='api_private_receptionist_profile_create_by_doctor'),
