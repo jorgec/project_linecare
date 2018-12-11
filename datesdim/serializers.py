e5119c616831c1936348dfd34224d9cbb6502d84
Jorge Cosgayon <jorge.cosgayon@gmail.com>
2018-12-11 14:07:39 +0800

    Moved repr_month to datedim

--- datesdim/serializers.py
+++ datesdim/serializers.py
@@ -5,24 +5,36 @@ from datesdim.models import DateDim, TimeDim
 
 
 class DateDimSerializer(serializers.ModelSerializer):
+    month_name = serializers.SerializerMethodField('repr_month_name')
+
+    def repr_month_name(self, obj):
+        return MONTH_CHOICES[obj.month - 1][1]
+
     class Meta:
         model = DateDim
-        fields = '__all__'
+        fields = (
+            'id',
+            'year',
+            'month',
+            'day',
+            'date_obj',
+            'day_name',
+            'week_day',
+            'week_month',
+            'week_year',
+            'month_name'
+        )
 
 
 class TimeDimSerializer(serializers.ModelSerializer):
     format_24 = serializers.SerializerMethodField('repr_format_24')
     format_12 = serializers.SerializerMethodField('repr_format_12')
-    month_name = serializers.SerializerMethodField('repr_month_name')
 
     def repr_format_24(self, obj):
         return obj.as_string()
 
     def repr_format_12(self, obj):
         return obj.format_12()
-
-    def repr_month_name(self, obj):
-        return MONTH_CHOICES[obj.month - 1][1]
 
     class Meta:
         model = TimeDim
@@ -32,5 +44,4 @@ class TimeDimSerializer(serializers.ModelSerializer):
             'minutes_since',
             'format_24',
             'format_12',
-            'month_name'
         )
