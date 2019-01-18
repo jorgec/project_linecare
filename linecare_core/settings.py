3b859c5c215ee640095dcd0ed6b2660da32ee8d6
Jorge Cosgayon <jorge.cosgayon@gmail.com>
2019-01-18 16:08:42 +0800

    Auto stash before merge of "dev-jorge" and "origin/dev"

--- linecare_core/settings.py
+++ linecare_core/settings.py
@@ -3,8 +3,7 @@ from split_settings.tools import optional, include
 
 if path.isfile("/home/ubuntu/envars/neo"):
     include("settings_collection/neo.py")
-elif path.isfile("/home/ubuntu/linecare/envars/local"):
-if path.isfile("/home/vagrant/local-jorge"):
+elif path.isfile("/home/vagrant/local-jorge"):
     include("settings_collection/local-jorge.py")
 elif path.isfile("/home/ubuntu/linecare/envars/local"):
     include("settings_collection/local.py")
