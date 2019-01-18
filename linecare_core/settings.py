3d8de10796850a0f99531de78877446211951e84
Neo Jay Andong <asakurayow21@yahoo.com>
2019-01-18 10:38:31 +0800

    Added neo settings

--- linecare_core/settings.py
+++ linecare_core/settings.py
@@ -1,7 +1,9 @@
 from os import path
 from split_settings.tools import optional, include
 
-if path.isfile("/home/ubuntu/linecare/envars/local"):
+if path.isfile("/home/ubuntu/envars/neo"):
+    include("settings_collection/neo.py")
+elif path.isfile("/home/ubuntu/linecare/envars/local"):
     include("settings_collection/local.py")
 elif path.isfile("/home/linecare/envars/testing"):
     include("settings_collection/testing.py")
